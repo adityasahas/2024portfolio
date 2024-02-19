3246e7735124648bdba498855668e6869bf352b7
Aditya Sahasranam <contact@adityasahas.tech>
2024-02-19 13:35:52 -0800

    sids the thinker

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,9 +1,10 @@
+"use client"
 import React from "react";
 import { FaGithub, FaLinkedin, FaInstagram, FaTwitter } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { Link } from "@nextui-org/react";
 import { IoDocumentText } from "react-icons/io5";
-
+import { Button } from "@nextui-org/react";
 export default function Socials() {
   const socials = [
     {
@@ -41,22 +42,25 @@ export default function Socials() {
   return (
     <div className="mt-5 grid grid-cols-2 gap-4 md:grid-cols-none md:flex md:flex-row md:space-x-4 md:space-y-0 max-w-6xl mx-auto justify-center md:justify-between">
       {socials.map((social, index, arr) => (
-        <div
+        <Button
+        isIconOnly
+        size="lg"
           key={social.name}
           className={`px-4 py-4 transform-gpu dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] rounded-xl flex flex-row items-center gap-2 w-full ${
             arr.length % 2 !== 0 && index === arr.length - 1 ? "col-span-2" : ""
           }`}
+          onClick={() => window.open(social.url, "_blank")}
         >
-          <social.icon size={30} color="#FFF" />
-          <Link
+          <social.icon size={30} color="#FFF" className="mx-auto" />
+          {/* <Link
             href={social.url}
             className="text-white ml-2"
             isExternal
             showAnchorIcon
           >
             {social.name}
-          </Link>
-        </div>
+          </Link> */}
+        </Button>
       ))}
     </div>
   );
